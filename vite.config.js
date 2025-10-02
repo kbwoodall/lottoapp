3ce720f53e57c197beac9083a5dce29f8e51ea99
Kerry Woodall <kerry.woodall@gmail.com>
2025-10-02 16:40:54 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base:https://kbwoodall.github.io/lottoapp,
   plugins: [react()],
 })
